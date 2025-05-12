73efd108199bcd6c9d4880aefe9c68e6c6c6a403
sravani-collab <sravani98001@gmail.com>
2025-05-12 12:33:55 +0530

    completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
 // app/layout.tsx
 import './globals.css';
 import { ReactNode } from 'react';
-
+import Link from 'next/link';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="p-6">
-        <nav className="mb-6">
-          <a href="/upload" className="text-sky-600 underline mr-4">Go to Upload Page</a>
-          <a href="/payment" className="text-sky-600 underline">Go to Payment Form</a>
+        <nav className="mb-6 space-x-4">
+          <Link href="/upload" className="text-sky-600 underline">Go to Upload Page</Link>
+          <Link href="/payment" className="text-sky-600 underline">Go to Payment Form</Link>
+          <Link href="/subscription" className="text-sky-600 underline">Go to Subscription Page</Link>
+          <Link href="/login" className="text-sky-600 underline">Go to login Page</Link>
         </nav>
         {children}
       </body>
